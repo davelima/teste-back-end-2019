026a92fca0ba8693a7815385279f03ff5cb50d2d
David Lima <contato@davidlima.com.br>
2019-07-10 18:45:37 -0300

    Add default user seeder

--- site/database/seeds/UsersTableSeeder.php
+++ site/database/seeds/UsersTableSeeder.php
@@ -1,9 +1,25 @@
 <?php
 
+use App\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class UsersTableSeeder extends Seeder
 {
+    /**
+     * Default user email
+     *
+     * @var string
+     */
+    const DEFAULT_USER_EMAIL = 'suporte@dindigital.com';
+
+    /**
+     * Default user password
+     *
+     * @var string
+     */
+    const DEFAULT_USER_PASSWORD = 'secret';
+
     /**
      * Run the database seeds.
      *
@@ -12,5 +28,28 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         factory(App\User::class, 1)->create();
+
+        $this->createDefaultUser();
+    }
+
+    /**
+     * Create default user if it doesn't exists yet
+     */
+    private function createDefaultUser()
+    {
+        $user = new User();
+
+        $defaultUserExists = User::query()
+            ->where('email', '=', self::DEFAULT_USER_EMAIL)
+            ->exists();
+
+        if (! $defaultUserExists) {
+            $user->name = 'Din Digital';
+            $user->email = self::DEFAULT_USER_EMAIL;
+            $user->email_verified_at = now();
+            $user->password = password_hash(self::DEFAULT_USER_PASSWORD, \PASSWORD_DEFAULT);
+            $user->remember_token = Str::random(10);
+            $user->save();
+        }
     }
 }
